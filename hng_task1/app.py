8613ad4afef3e06f38d0747b279439709dd670cf
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-09-11 08:29:36 +0000

    reconfigured the time

--- hng_task1/app.py
+++ hng_task1/app.py
@@ -4,36 +4,6 @@ from datetime import datetime as dt, timedelta
 app = Flask(__name__)
 
 
-def current_date_time():
-    """excutes the utc date and time"""
-
-
-    now = dt.utcnow()
-    WAT_now = now.strftime('%Y-%m-%dT%k:%M:%SZ')
-    #string = dt.isoformat(WAT_now)
-    return WAT_now
-
-
-def current_day():
-    """excutes the current day in full"""
-
-
-    now = dt.utcnow()
-    WAT_now = now + timedelta(hours = 1)
-    day = WAT_now.strftime("%A")
-    return day
-
-
-details = {
-            "slack_name": "Codeslinger",
-            "current_day": current_day(),
-            "utc_time": current_date_time(),
-            "track": "backend",
-            "github_file_url": "https://github.com/barth007/scaling-fortnight/hng_task1/app.py",
-            "github_repo_url": "https://github.com/barth007/scaling-fortnight",
-            "status_code": 200
-        }
-
 
 @app.route('/hng/api', methods=['GET'])
 def get_all():
@@ -42,10 +12,22 @@ def get_all():
 
     slack_name = request.args.get('slack_name')
     track = request.args.get('track')
-    if slack_name != details['slack_name'] or track != details['track']:
-        abort(not_found(404))
+    current_day = dt.utcnow().strftime('%A')
+    current_date_time = dt.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    github_file_url = "https://github.com/barth007/scaling-fortnight/hng_task1/app.py"
+    github_repo_url =  "https://github.com/barth007/scaling-fortnight"
     if slack_name is None or track is None:
         abort(bad_request(400))
+
+    details = {
+                "slack_name": slack_name,
+                "current_day": current_day,
+                "utc_time": current_date_time,
+                "track": track,
+                "github_file_url": github_file_url,
+                "github_repo_url": github_repo_url,
+                "status_code": 200
+            }
     return jsonify(details)
 
 
@@ -55,14 +37,6 @@ def bad_request(error):
 
 
     return make_response(jsonify({'error': 'Bad Request'}), 400)
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """error for wrong input or None existing values"""
-
-
-    return make_response(jsonify({'error': 'Not Found'}))
 
 
 if __name__ == '__main__':
